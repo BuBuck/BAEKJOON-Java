c2e03e66f7a9c319fc72c6c1a7d9fb48b95c3e5f
BuBuck <sunjeongin123@gmail.com>
2025-08-03 02:09:10 +0900

    feat: 1000 A+B

---
+++ ex1/Ex2_1000.java
@@ -0,0 +1,12 @@
+package ex1;
+
+import java.util.Scanner;
+
+public class Ex2_1000 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt(), b = input.nextInt();
+
+        System.out.println(a + b);
+    }
+}
