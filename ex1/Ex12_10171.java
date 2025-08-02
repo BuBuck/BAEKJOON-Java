fd33e1dda55cbc6447d026e9001a6c67e6368f97
BuBuck <sunjeongin123@gmail.com>
2025-08-03 04:00:40 +0900

    feat: 10171 고양이

---
+++ ex1/Ex12_10171.java
@@ -0,0 +1,10 @@
+package ex1;
+
+public class Ex12_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
