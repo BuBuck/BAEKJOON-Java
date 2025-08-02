c2ca9c47145e27f4e3a3f2e7421e2de888aa8db0
BuBuck <sunjeongin123@gmail.com>
2025-08-03 02:38:59 +0900

    feat: 10926 ??!

---
+++ ex1/Ex7_10926.java
@@ -0,0 +1,13 @@
+package ex1;
+
+import java.util.Scanner;
+
+public class Ex7_10926 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String id = input.nextLine();
+        id += "??!";
+        System.out.println(id);
+    }
+}
