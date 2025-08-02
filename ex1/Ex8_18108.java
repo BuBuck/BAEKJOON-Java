b7d10b9e1690a206a3ff69d4ef6261538326df94
BuBuck <sunjeongin123@gmail.com>
2025-08-03 02:43:43 +0900

    feat: 18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ ex1/Ex8_18108.java
@@ -0,0 +1,12 @@
+package ex1;
+
+import java.util.Scanner;
+
+public class Ex8_18108 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int year = input.nextInt();
+        System.out.println(year - 543);
+    }
+}
