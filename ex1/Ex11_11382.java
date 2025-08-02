041e95158efc30f0ea87e78bcae5c5e30a882077
BuBuck <sunjeongin123@gmail.com>
2025-08-03 03:55:49 +0900

    feat: 11382 꼬마 정민

---
+++ ex1/Ex11_11382.java
@@ -0,0 +1,12 @@
+package ex1;
+
+import java.util.Scanner;
+
+public class Ex11_11382 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long a = input.nextLong(), b = input.nextLong(), c = input.nextLong();
+
+        System.out.println(a + b + c);
+    }
+}
