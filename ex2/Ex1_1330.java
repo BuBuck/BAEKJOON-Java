bdc9232865f57b180ff93a4c261d54e9ec876565
BuBuck <sunjeongin123@gmail.com>
2025-08-04 20:33:30 +0900

    feat: 1330 두 수 비교하기

---
+++ ex2/Ex1_1330.java
@@ -0,0 +1,19 @@
+package ex2;
+
+import java.util.Scanner;
+
+public class Ex1_1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt(), b = sc.nextInt();
+
+        if (a > b) {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        }  else {
+            System.out.println("==");
+        }
+    }
+}
