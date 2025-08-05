bc6fc179b69d0a174c6e87c458789b9db670e20a
BuBuck <sunjeongin123@gmail.com>
2025-08-06 00:53:19 +0900

    feat: 10950 A+B - 3

---
+++ ex3/Ex2_10950.java
@@ -0,0 +1,19 @@
+package ex3;
+
+import java.util.Scanner;
+
+public class Ex2_10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        int[] a = new int[t], b = new int[t];
+
+        for (int i = 0; i < t; i++) {
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
+
+            System.out.println(a[i] + b[i]);
+        }
+    }
+}
