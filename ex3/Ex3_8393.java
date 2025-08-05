3373b5e34f1f15a683099556090b2a72745f7b6b
BuBuck <sunjeongin123@gmail.com>
2025-08-06 01:02:32 +0900

    feat: 8393 합

---
+++ ex3/Ex3_8393.java
@@ -0,0 +1,13 @@
+package ex3;
+
+import java.util.Scanner;
+
+public class Ex3_8393 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n =  sc.nextInt();
+
+        System.out.println((n + 1) * n / 2);
+    }
+}
