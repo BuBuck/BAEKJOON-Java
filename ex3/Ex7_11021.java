d1539dc04b64823b023d53e0d36dd9004698b10f
BuBuck <sunjeongin123@gmail.com>
2025-08-08 00:55:10 +0900

    feat: 11021 A+B - 7

---
+++ ex3/Ex7_11021.java
@@ -0,0 +1,17 @@
+package ex3;
+
+import java.util.Scanner;
+
+public class Ex7_11021 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println("Case #" + (i + 1) + ": " + (a + b));
+        }
+    }
+}
